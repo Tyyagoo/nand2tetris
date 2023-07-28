1f9239c5c2988b3c6a65255d69eae0e1784d9c60
Tyyagoo <tyyago.dev@gmail.com>
2023-07-28 13:29:52 -0300

    [Ch4] Impl Mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,54 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    @i
+    M=1
+    @R2
+    M=0
+    @R0
+    D=M
+    @R1
+    D=D-M
+    @GTR
+    D;JGT
+    @LSR
+    0;JMP
+
+(GTR)
+    @x
+    M=0
+    @y
+    M=1
+    @LOOP
+    0;JMP
+
+(LSR)
+    @x
+    M=1
+    @y
+    M=0
+    @LOOP
+    0;JMP
+
+(LOOP)
+    @y
+    A=M
+    D=M
+    @i
+    D=M-D
+    @END
+    D;JGT
+
+    @x
+    A=M
+    D=M
+    @R2
+    M=D+M
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
