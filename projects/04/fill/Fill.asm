1186b5abd48543780ac6e33bfb0102454cb18a34
Tyyagoo <tyyago.dev@gmail.com>
2023-07-28 14:43:40 -0300

    [Ch4] Impl Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,58 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(SETUP)
+    @SCREEN
+    D=A
+    @R0
+    M=D
+    @8192
+    D=A
+    @SCREEN
+    D=D+A
+    @R1
+    M=D
+    @POLLING
+    0;JMP
+
+(POLLING)
+    @KBD
+    D=M
+    @WHITE
+    D;JEQ
+    @BLACK
+    0;JMP
+
+
+(WHITE)
+    @R2
+    M=0
+    @FILL
+    0;JMP
+
+(BLACK)
+    @R2
+    M=-1
+    @FILL
+    0;JMP
+
+(FILL)
+    @R1
+    D=M
+    @R0
+    D=D-M
+    @SETUP
+    D;JEQ
+
+    @R2
+    D=M
+    @R0
+    A=M
+    M=D
+
+    @R0
+    M=M+1
+
+    @POLLING
+    0;JMP
